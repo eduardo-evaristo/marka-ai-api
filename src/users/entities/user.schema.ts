a66980475148b29241e3400aeafd3bcbee394be6
Eduardo Evaristo <eduardo.evaristo.am@gmail.com>
2025-04-26 00:50:04 -0300

    feat: update user schema to add displayName field and make username unique

--- src/users/entities/user.schema.ts
+++ src/users/entities/user.schema.ts
@@ -2,8 +2,11 @@ import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 
 @Schema()
 export class User {
-  @Prop({ required: true })
+  @Prop({ required: true, unique: true })
   username: string;
+
+  @Prop({ required: true })
+  displayName: string;
 
   @Prop({ required: true, select: false })
   password: string;
