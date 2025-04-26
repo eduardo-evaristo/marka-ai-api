8dc423f00f2d2c71654ed7533d66a1ae7b2d45e7
Eduardo Evaristo <eduardo.evaristo.am@gmail.com>
2025-04-26 01:31:11 -0300

    feat: export usersServices and import UserModule into AuthModule

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,6 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AuthServices } from './auth.service';
 import { AuthController } from './auth.controller';
+import { UsersModule } from 'src/users/users.module';
 
-@Module({ controllers: [AuthController], providers: [AuthServices] })
+@Module({
+  imports: [UsersModule],
+  controllers: [AuthController],
+  providers: [AuthServices],
+})
 export class AuthModule {}
--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -10,5 +10,6 @@ import { User, UserSchema } from './entities/user.schema';
   ],
   controllers: [UsersController],
   providers: [UsersServices],
+  exports: [UsersServices],
 })
 export class UsersModule {}
