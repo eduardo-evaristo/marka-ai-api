ecb0fd97a433f047389ea8f6a2d99ecdbd494c11
Eduardo Evaristo <eduardo.evaristo.am@gmail.com>
2025-04-25 23:38:23 -0300

    feat: create endpoints for posting and fetching meetings

--- src/meetings/meetings.controller.ts
+++ src/meetings/meetings.controller.ts
@@ -1,4 +1,22 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { MeetingsServices } from './meetings.service';
+import { createMeetingDTO } from './dtos/create-meeting.dto';
 
 @Controller('meetings')
-export class MeetingsController {}
+export class MeetingsController {
+  constructor(private meetingsServices: MeetingsServices) {}
+
+  @Get()
+  fetch() {
+    return this.meetingsServices.fetch();
+  }
+  @Post()
+  create(@Body() createMeetingDTO: createMeetingDTO) {
+    return this.meetingsServices.create(createMeetingDTO);
+  }
+
+  @Get(':id')
+  fetchOne(@Param('id') meetingId: string) {
+    return this.meetingsServices.fetchOne(meetingId);
+  }
+}
--- src/meetings/meetings.service.ts
+++ src/meetings/meetings.service.ts
@@ -1,4 +1,30 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Meeting, MeetingSchema } from './entities/meeting.schema';
+import { Model } from 'mongoose';
+import { createMeetingDTO } from './dtos/create-meeting.dto';
+import { AssociatedUser } from 'src/users/entities/associated-user.schema';
 
 @Injectable()
-export class MeetingsServices {}
+export class MeetingsServices {
+  constructor(
+    @InjectModel(Meeting.name) private meetingModel: Model<Meeting>,
+  ) {}
+  async create(createMeetingDTO: createMeetingDTO) {
+    const newMeeting = new this.meetingModel(createMeetingDTO);
+    return newMeeting.save();
+  }
+
+  async fetchOne(meetingId: string) {
+    const meeting = await this.meetingModel.findById(meetingId).populate<{
+      associatedPeople: AssociatedUser;
+    }>('associatedPeople.user');
+    return meeting;
+  }
+
+  async fetch() {
+    // TODO: This is likely gonna be used to display all the ones we currently have, so likely no need to fetch the associatedPeople in this one
+    const meetings = await this.meetingModel.find();
+    return meetings;
+  }
+}
