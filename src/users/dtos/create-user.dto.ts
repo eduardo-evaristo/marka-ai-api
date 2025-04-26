69a595fbbc945d559e08287e5fc774ab255d9453
Eduardo Evaristo <eduardo.evaristo.am@gmail.com>
2025-04-26 01:03:48 -0300

    feat: update CreateUserDTO to include optional displayName and confirmPassword fields

--- src/users/dtos/create-user.dto.ts
+++ src/users/dtos/create-user.dto.ts
@@ -1,11 +1,20 @@
-import { IsNotEmpty, IsString } from 'class-validator';
+import { IsNotEmpty, IsOptional, IsString } from 'class-validator';
 
 export class CreateUserDTO {
   @IsString()
   @IsNotEmpty()
   username: string;
 
+  @IsOptional()
+  @IsString()
+  @IsNotEmpty()
+  displayName?: string;
+
   @IsString()
   @IsNotEmpty()
   password: string;
+
+  @IsString()
+  @IsNotEmpty()
+  confirmPassword: string;
 }
